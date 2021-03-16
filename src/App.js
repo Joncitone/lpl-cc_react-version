56414a4370b2968811e6e37da655dfe39822b8e6
Joncitone <jarreola7123@gmail.com>
2021-03-16 12:43:03 -0500

    fixed dom element nesting violations

--- src/App.js
+++ src/App.js
@@ -4,73 +4,65 @@ function App() {
   let codeText = '< coding challenge />';
   return (
     <div className="App">
-      <head>
-        <meta charset="UTF-8" />
-        <title>LaunchPad Lab {codeText}</title>
-        <link rel="icon" type="image" href="public/favicon.ico" />
-        <link rel="stylesheet" href="css/style.css" />
-      </head>
-      <body>
-        <header>
-          <div id="home-page-header">
-            <p>
-              <b>LaunchPad Lab</b>
-            </p>
-            <p id="code-text">{codeText}</p>
-          </div>
-        </header>
-        <br />
-        <main>
-          <div>
-            <table id="table-container">
-              <thead>
-                <tr>
-                  <th id="framework-header">Framework</th>
-                  <th id="score-header">Score</th>
-                  <th id="forks-header">Forks</th>
-                  <th id="stars-header">Stars</th>
-                  <th id="issues-header">Issues (open)</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr id="row-0">
-                  <td>React</td>
-                  <td id="react-score">score</td>
-                  <td id="react-forks">forks</td>
-                  <td id="react-stars">stars</td>
-                  <td id="react-issues">issues</td>
-                </tr>
-                <tr id="row-1">
-                  <td>Angular</td>
-                  <td id="angular-score">score</td>
-                  <td id="angular-forks">forks</td>
-                  <td id="angular-stars">stars</td>
-                  <td id="angular-issues">issues</td>
-                </tr>
-                <tr id="row-2">
-                  <td>Ember</td>
-                  <td id="ember-score">score</td>
-                  <td id="ember-forks">forks</td>
-                  <td id="ember-stars">stars</td>
-                  <td id="ember-issues">issues</td>
-                </tr>
-                <tr id="row-3">
-                  <td>Vue</td>
-                  <td id="vue-score">score</td>
-                  <td id="vue-forks">forks</td>
-                  <td id="vue-stars">stars</td>
-                  <td id="vue-issues">issues</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </main>
-        <br />
-        <footer>
-          <p>made with React by Jonathan Arreola</p>
-        </footer>
-        <script type="module" src="js/main.js"></script>
-      </body>
+      <header>
+        <div id="home-page-header">
+          <p>
+            <b>LaunchPad Lab</b>
+          </p>
+          <p id="code-text">{codeText}</p>
+        </div>
+      </header>
+      <br />
+      <main>
+        <div>
+          <table id="table-container">
+            <thead>
+              <tr>
+                <th id="framework-header">Framework</th>
+                <th id="score-header">Score</th>
+                <th id="forks-header">Forks</th>
+                <th id="stars-header">Stars</th>
+                <th id="issues-header">Issues (open)</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr id="row-0">
+                <td>React</td>
+                <td id="react-score">score</td>
+                <td id="react-forks">forks</td>
+                <td id="react-stars">stars</td>
+                <td id="react-issues">issues</td>
+              </tr>
+              <tr id="row-1">
+                <td>Angular</td>
+                <td id="angular-score">score</td>
+                <td id="angular-forks">forks</td>
+                <td id="angular-stars">stars</td>
+                <td id="angular-issues">issues</td>
+              </tr>
+              <tr id="row-2">
+                <td>Ember</td>
+                <td id="ember-score">score</td>
+                <td id="ember-forks">forks</td>
+                <td id="ember-stars">stars</td>
+                <td id="ember-issues">issues</td>
+              </tr>
+              <tr id="row-3">
+                <td>Vue</td>
+                <td id="vue-score">score</td>
+                <td id="vue-forks">forks</td>
+                <td id="vue-stars">stars</td>
+                <td id="vue-issues">issues</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </main>
+      <br />
+      <footer>
+        <p>made with React by Jonathan Arreola</p>
+      </footer>
+      <script type="module" src="js/main.js"></script>
     </div>
   );
 }
